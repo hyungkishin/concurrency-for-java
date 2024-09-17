361a2193cef820370fe52eb8efc427c1ddbcb31d
hyungkishin <tkaqkeldk@nate.com>
2024-09-17 20:41:52 +0900

    feat: 데몬 스레드 - user 스레드인 main 스레드가 setDaemon() true/false 값에 따라 즉시 종료가 되거나, 의도한대로 움직인다. 데몬 스레드가 true 인 경우는 모든 사용자 스레드가 종료되면 강제로 종료된다. 반면 사용자 스레드는 모든 사용자 스레드가 종료될 때 까지 JVM 이 종료되지 않는다

---
+++ src/thread/start/DaemonThreadMain.java
@@ -0,0 +1,30 @@
+package thread.start;
+
+public class DaemonThreadMain {
+
+    public static void main(String[] args) {
+        System.out.println(Thread.currentThread().getName() + ": main() start");
+        DaemonThread daemonThread = new DaemonThread();
+        daemonThread.setDaemon(false);
+        daemonThread.start();
+        System.out.println(Thread.currentThread().getName() + ": main() end");
+    }
+
+    static class DaemonThread extends  Thread {
+
+        @Override
+        public void run() {
+            System.out.println(Thread.currentThread().getName() + ": run()");
+
+            try {
+                Thread.sleep(10000);
+            } catch (InterruptedException e) {
+                System.out.println("e = " + e);
+                throw new RuntimeException(e);
+            }
+
+            System.out.println(Thread.currentThread().getName() + ": end()");
+        }
+    }
+
+}
