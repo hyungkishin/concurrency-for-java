a560c5a998b23c01d58eb7ec7faaa074f75a210c
hyungkishin <tkaqkeldk@nate.com>
2024-09-24 13:36:04 +0900

    feat: 성능을 약간 포기하는 대신에, 값을 읽을때 모두 메밍 메모리에 직접 접근하는 방식인 volatile 키워드 사용     - runFlag 에 대해서는 캐시 메모리를 사용하지 않고, 값을 읽거나 쓸 때 항상 메인 메모리에 직접 접근하도록 변경 - 여러 쓰레드 에서 같은 값을 읽기 때문에 캐시 메모리를 사용할때 보다 성능이 느려지는 단점이 존재.

---
+++ src/thread/volatiles/VolatileFlagMain2.java
@@ -0,0 +1,38 @@
+package thread.volatiles;
+
+import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
+
+public class VolatileFlagMain2 {
+
+    public static void main(String[] args) {
+        MyTask task = new MyTask();
+        Thread thread = new Thread(task, "work");
+        log("runFlag = " + task.runFlag);
+        thread.start();
+
+        sleep(1000);
+        log("runFlag 를 false 로 변경 시도");
+        task.runFlag = false;
+        log("runFlag = " + task.runFlag);
+        log("main 종료");
+    }
+
+    static class MyTask implements Runnable {
+
+        volatile boolean runFlag = true;
+
+        @Override
+        public void run() {
+            log("task 시작");
+
+            while (runFlag) {
+                // runFlag 가 false 로 변하면 탈출
+            }
+
+            log("task 종료");
+        }
+
+
+    }
+}
