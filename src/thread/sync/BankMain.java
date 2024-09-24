c2caab9b93f9c8d34c965a147594483000ef8d2e
hyungkishin <tkaqkeldk@nate.com>
2024-09-24 15:11:25 +0900

    feat: 동시성 문제 발생 - volatile 을 도입해도, 해결되지 않는다. 이유는, 한 스레드가 값을 변경했을 때, 다른 스레드에서 변경된 값을 즉시 볼 수 있게 하는 메모리 가시성의 문제를 해결하지만, 전혀 다른 문제인 동시성 문제이기 때문에 최종 잔액이 마이너스 로 출력된다.

---
+++ src/thread/sync/BankMain.java
@@ -0,0 +1,25 @@
+package thread.sync;
+
+import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
+
+public class BankMain {
+
+    public static void main(String[] args) throws InterruptedException {
+        BankAccountV1 account = new BankAccountV1(1000);
+
+        Thread t1 = new Thread(new WithDrawTask(account, 800), "t1");
+        Thread t2 = new Thread(new WithDrawTask(account, 800), "t2");
+
+        sleep(500);
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+
+        log("최종 잔액: " + account.getBalance());
+    }
+
+}
---
+++ src/thread/sync/WithDrawTask.java
@@ -0,0 +1,18 @@
+package thread.sync;
+
+public class WithDrawTask implements Runnable {
+
+    private BankAccount account;
+
+    private int amount;
+
+    public WithDrawTask(BankAccount account, int amount) {
+        this.account = account;
+        this.amount = amount;
+    }
+
+    @Override
+    public void run() {
+        account.withdraw(amount);
+    }
+}
