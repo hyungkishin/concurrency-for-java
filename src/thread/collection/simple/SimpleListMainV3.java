08deadfa7257ffb49f28985afbdeac58a5659888
신형기 <tkaqkeldk@nate.com>
2024-11-06 00:11:04 +0900

    feat: add() 메서드에 synchronized 를 사용해서 안전한 임계영역을 만들어 해결

---
+++ src/thread/collection/simple/SimpleListMainV3.java
@@ -0,0 +1,45 @@
+package thread.collection.simple;
+
+import static util.MyLogger.log;
+
+import thread.collection.simple.list.BasicList;
+import thread.collection.simple.list.SyncList;
+
+public class SimpleListMainV3 {
+
+  public static void main(String[] args) throws InterruptedException {
+    test(new SyncList());
+  }
+
+  private static void test(SyncList list) throws InterruptedException {
+    log(list.getClass().getSimpleName());
+
+    Runnable addA = new Runnable() {
+      @Override
+      public void run() {
+        list.add("A");
+        log("Thread-1: list.add(A)");
+      }
+    };
+
+    // B를 리스트에 저장하는 코드
+    Runnable addB = new Runnable() {
+      @Override
+      public void run() {
+        list.add("B");
+        log("Thread-1: list.add(B)");
+      }
+    };
+
+    Thread t1 = new Thread(addA, "Thread-1");
+    Thread t2 = new Thread(addB, "Thread-2");
+    t1.start();
+    t2.start();
+
+    t1.join();
+    t2.join();
+
+    log(list);
+  }
+
+}
